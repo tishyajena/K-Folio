30e3cfbbcfa80764867c38f504ea0000bfe67f04
garimakumarix29 <garimakumari.x29@gmail.com>
2026-01-02 15:02:45 +0530

    Landing page setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
+import LandingPage from "./pages/Landing/LandingPage"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold">K-Folio</h1>
-      <div className="card">
-      </div>
-      <p className="read-the-docs">
-        Social media platform for kiit.
-      </p>
+      <LandingPage />
     </>
   )
 }
